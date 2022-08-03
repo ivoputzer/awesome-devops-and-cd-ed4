d2a95cd8afbe70dde96f02583c086f8a9bb69f41
ivoputzer <ivo.putzer@gmail.com>
2022-08-03 14:34:07 +0000

    Deploying to gh-pages from @ ivoputzer/awesome-devops-and-cd-ed4@37a23e6ef5bb33155cee1f0c6dd50573ef900d70 🚀

--- github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/test/index.mjs
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/test/index.mjs
@@ -1,8 +1,8 @@
-import {equal} from 'assert'
+import {ok} from 'assert'
 import battleship from '../index.mjs'
 
 describe('battleship', () => {
   it('should be defined', () => {
-    equal(typeof battleship, 'function')
+    ok(battleship)
   })
 })
